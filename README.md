9d723f4b815f71c00832d56129871b625eefcb1f
Vladimir Grinenko <i@tadatuta.com>
2015-05-01 22:53:09 +0300

    initial

---
+++ README.md
@@ -0,0 +1,36 @@
+var levels = [
+    '../../libs/bem-components/common.blocks',
+    '../../libs/bem-components/desktop.blocks',
+    '../../libs/bem-components/design/common.blocks',
+    '../../libs/bem-components/design/desktop.blocks'
+];
+
+// TODO: рекурсивно погружаться вглубь директорий ?
+// TODO: добавлять поле про файл/директория
+
+var Valkyrie = require('./valkyrie'),
+    valka = Valkyrie(levels);
+
+valka
+    .on('*', function(files) {
+        // все файлы любых сущностей
+    })
+    .on('block', function(files) {
+        // все файлы блоков
+    })
+    .on({ block: 'select' }, function(files) {
+        // все файлы блока select
+    })
+    .on({ tech: 'examples' }, function(files) {
+        // все файлы с технологией examples
+        Valkyrie([files.path], { scheme: 'flat' })
+            .on({ tech: 'blocks' }, function(files) {
+                Valkyrie([files.path])
+                    .on('*', function(files) {
+                        console.log(files); // все файлы кастомных блоков для примеров
+                    })
+            });
+    })
+    .on('end', function(files) {
+        console.log('end');
+    });
---
+++ index.js
@@ -0,0 +1,60 @@
+var util = require('util'),
+    EventEmitter = require('events').EventEmitter,
+    bemWalk = require('bem-walk'),
+    naming = require('bem-naming');
+
+util.inherits(Valkyrie, EventEmitter);
+
+function Valkyrie(levels, opts) {
+    if (!(this instanceof Valkyrie)) {
+        return new Valkyrie(levels, opts);
+    }
+
+    var _this = this,
+        walker = this.walker = bemWalk(levels, opts);
+
+    walker
+        .on('data', function(item) {
+            var type = naming.typeOf(item.entity),
+                techKey = { tech: item.tech },
+                entityKey = {};
+
+            entityKey[type] = item.entity[type];
+
+            _this.emit('*', item);
+            _this.emit(type, item);
+            _this.emit(JSON.stringify(entityKey), item);
+            _this.emit(JSON.stringify(techKey), item);
+        })
+        .on('end' ,function() {
+            _this.emit('end');
+        })
+        .on('error', function(err) {
+            _this.emit('error', err);
+        });
+
+    this.on = function(e, cb) {
+        if (typeof e === 'object') e = JSON.stringify(e);
+        this.constructor.prototype.on.call(this, e, cb);
+        return this;
+    }
+
+    this.get = function(e, field, cb) {
+        if (arguments.length < 3) {
+            cb = field;
+            field = null;
+        }
+
+        var res = [];
+
+        return this
+            .on(e, function(files) {
+                res.push(field ? files[field] : files);
+            })
+            .on('end', function() {
+                cb(res);
+            });
+    }
+}
+
+module.exports = Valkyrie;
